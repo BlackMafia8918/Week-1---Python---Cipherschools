a5df270c4b1ef6714b778419f273d21fbdeee3c5
Adesh Yadav <adesh.lpu@gmail.com>
2022-12-01 23:52:13 +0530

    Add files via upload

---
+++ Lecture 6 - Command Line tutorial.py
@@ -0,0 +1,2 @@
+print('hello world')
+print("Adesh Yadav")
---
+++ Lecture 7 - Python Chapter 0 Summary.py
@@ -0,0 +1,8 @@
+mv - renaming a file /copying a file 
+mv "file name"  - to move 
+mv "old file name" "new file name" - to rename a file 
+
+mkdir - creating a new directory
+
+cp - copy a File 
+
